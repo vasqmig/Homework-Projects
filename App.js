ec3df35f2f23b4434aaa8a96fc8a304504f486ff
Miguel V <126122762+vasqmig@users.noreply.github.com>
2023-07-30 22:03:01 -0700

    26.1 react forms

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BoxList from "./TodoList";
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
