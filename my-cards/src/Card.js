0cf0ab7956065a415db31d0346fec4e788b9087b
Miguel V <126122762+vasqmig@users.noreply.github.com>
2023-08-06 20:36:59 -0700

    26.11

---
+++ my-cards/src/Card.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import "./Card.css";
+
+function Card({name, image}) {
+    const [{angle,x,y}] = useState({
+        angle: Math.random() *90 - 45,
+        x: Math.random() * 40 - 20,
+        y: Math.random() * 40 - 20,
+    });
+
+    const transform = `translate(${x}px, ${y}px) rotate(${angle}deg)`;
+
+    return <img
+    className='Card'
+    alt={name}
+    src={image}
+    style = {{transform}} />;
+}
+
+export default Card; 
